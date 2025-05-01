668c5b995d5c6b4b7d8edd1878248549bd60b810
Marlon Colca <marlon.colca.io@gmail.com>
2025-05-01 08:41:59 -0500

    fin

---
+++ src/components/Greeting.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+
+const props = defineProps({
+  messages: {
+    type: Array<string>,
+    required: true,
+  },
+})
+const randomMessage = () => props.messages[(Math.floor(Math.random() * props.messages.length))];
+const greeting = ref(randomMessage());
+
+function setGreeting () {
+  greeting.value = randomMessage()
+}
+
+</script>
+
+<template>
+  <div>
+      <h3>{{ greeting }}! Thank you for visiting!</h3>
+      <button @click="() => setGreeting()">
+        New Greeting
+      </button>
+    </div>
+</template>
+
